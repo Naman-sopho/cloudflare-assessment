5a1ac3e453ec713fd89e63a0b6faf3469d94f5b2
Naman-sopho <namantiwari.nt@gmail.com>
2020-04-15 00:45:25 -0400

    Finish extra credits task 1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
+ * Respond with html from one of the variants
  * @param {Request} request
  */
 async function handleRequest(request) {
@@ -14,11 +15,52 @@ async function handleRequest(request) {
   let random = Math.floor(Math.random() * (max - min) + min);
   
   let final_response = await fetchFromRequiredURL(response.variants[random], false);
-  return new Response(final_response, {
-    headers: { 'content-type': 'text/html' },
+  let ele = new ElementHandler();
+  let response_obj = new Response(final_response, {
+    headers: {'content-type': 'text/html'}
   })
+  let html = new HTMLRewriter().on('*', ele).transform(response_obj);
+  return html;
 }
 
+class ElementHandler {
+  element(element) {
+    // An incoming element, such as `div`
+
+    // Edit title of the page
+    if (element.tagName == 'title') {
+      element.setInnerContent('Naman Tiwari')
+    }
+
+    else if (element.tagName == 'h1' && element.getAttribute('id') == 'title') {
+      element.setInnerContent('Naman Tiwari')
+    }
+
+    else if (element.tagName == 'p' && element.getAttribute('id') == 'description') {
+      element.setInnerContent('Placeholder for some witty oneliner!!!')
+    }
+
+    else if (element.tagName == 'a' && element.getAttribute('id') == 'url') {
+      element.setInnerContent('Link to website')
+      element.setAttribute('href', 'https://naman-sopho.github.io')
+    }
+    
+  }
+
+  comments(comment) {
+    // An incoming comment
+  }
+
+  text(text) {
+    // An incoming piece of text
+  }
+}
+
+/**
+ * Generic function to handle simple fetch requests to an url.
+ * @param {*} url The url to which the fetch request is sent.
+ * @param {*} returnJson Returns json object if true else returns plain text.
+ */
 async function fetchFromRequiredURL(url, returnJson) {
   let response = await fetch(url);
 
